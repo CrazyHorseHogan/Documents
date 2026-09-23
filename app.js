ed665317d9f5fa15caac376a23668ee6474e5afb
CrazyHorseHogan <gerard.hogan98@gmail.com>
2026-09-23 22:34:39 +0100

    Update app.js

--- app.js
+++ app.js
@@ -39,7 +39,7 @@ const backToGarden =
 
 let plantViewFilter = "current";
 const WHOLE_GARDEN_VIEW =
-    "-350 0 6000 7000";
+    "-350 0 6700 7000";
 const connectionStatus =
     document.getElementById("connection-status");
 
